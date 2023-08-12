7a190a5885efd81f5737ddba10515663c272f357
spcswap <116253432+kesav1412@users.noreply.github.com>
2023-08-12 16:51:55 +0530

    Add files via upload

---
+++ spc.api.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+axios.get(url)
+  .then(response => {
+    const json = response.data;
+    parseJson(json);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+function parseJson(json) {
+  const usdValue = '1 BTC = $ ' + json.bpi.USD.rate;
+  const usdRate = parseFloat(json.bpi.USD.rate.replace(',', ''));
+  const spcValue = usdRate / 6500;
+  `SPC = ${spcValue.toFixed(4)} USD`;
+}
