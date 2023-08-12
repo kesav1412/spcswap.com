1348f2a9faee0db1fd98c543e311aa31d47eae4d
spcswap <116253432+kesav1412@users.noreply.github.com>
2023-08-12 17:41:48 +0530

    spc.api.js

--- spc.api.js
+++ spc.api.js
@@ -15,5 +15,5 @@ function parseJson(json) {
   const usdValue = '1 BTC = $ ' + json.bpi.USD.rate;
   const usdRate = parseFloat(json.bpi.USD.rate.replace(',', ''));
   const spcValue = usdRate / 6500;
-  `SPC = ${spcValue.toFixed(4)} USD`;
+  `spc = ${spcValue.toFixed(4)} USD`;
 }
